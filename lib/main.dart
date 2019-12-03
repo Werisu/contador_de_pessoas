2bc735a2f0f87ef5fe9e7886f988fac873a01770
Werisu <wellysson35@gmail.com>
2019-12-03 14:30:13 -0300

    Comentários

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// importação de todos widgets relacionados ao design
+import 'package:flutter/material.dart';
+
+//todo projeto começa pela função main
+void main(){
+
+  /**
+   * Comando para rodar o app (necessita das labarys)
+   */
+  runApp(MaterialApp(
+    title: "Contator de Pessoas",
+    home: Container(color: Colors.white,)
+  ));
+}
