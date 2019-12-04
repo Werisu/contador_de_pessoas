8f2f1ab2a94477da99f375c74009debd96ce6300
Werisu <wellysson35@gmail.com>
2019-12-03 21:51:37 -0300

    Estilizando, criando textos e botões

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,61 @@
 import 'package:flutter/material.dart';
 
 //todo projeto começa pela função main
-void main(){
-
+void main() {
   /**
    * Comando para rodar o app (necessita das labarys)
    */
   runApp(MaterialApp(
-    title: "Contator de Pessoas",
-    home: Container(color: Colors.white,)
-  ));
+      title: "Contator de Pessoas", //título usado apenas internamente
+      home: Stack(
+        children: <Widget>[
+          Image.asset(
+            "imagens/img1.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoas: 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "Pode Entrar!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
+              )
+            ],
+          )
+        ],
+      )));
 }
